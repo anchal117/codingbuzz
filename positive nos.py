4da768c5325f63490602f39c4e1e150e189cae02
anchal117 <65453778+anchal117@users.noreply.github.com>
2020-06-10 19:23:18 +0530

    Create positive nos.py

---
+++ positive nos.py
@@ -0,0 +1,8 @@
+#python program to print positive numbers in a range
+#list of numbers given
+list1=[12,-7,5,64,-14]
+#iterating statement
+for num in list1:
+#checking statement
+if num>=0:
+print(num);
