9321c1a23fe662550e9ff9e637a13215fe515f74
anchal117 <65453778+anchal117@users.noreply.github.com>
2020-06-10 19:55:41 +0530

    Create DS.py

---
+++ DS.py
@@ -0,0 +1,21 @@
+#Assigning elements to different lists
+list1=['hi','hello',hey']
+list2=['apple','mango']
+print(list1)
+print(list2)
+
+
+#Accessing elements from a tuple
+tuple=('a','b','c')
+print(tuple[0])
+print(tuple[1])
+print(tuple[2])
+
+
+
+#Deleting different dictionary elements
+dictionary={
+"mango":"tasty"'"pineapple":"yummy"
+}
+del dictionary["mango"]
+print(dictionary)
