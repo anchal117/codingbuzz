32ebc45be9f327ca2c06b3c4736108d1107a7736
anchal117 <65453778+anchal117@users.noreply.github.com>
2020-06-18 19:35:37 +0530

    Update positive nos.py

--- positive nos.py
+++ positive nos.py
@@ -1,8 +1,54 @@
-#python program to print positive numbers in a range
-#list of numbers given
-list1=[12,-7,5,64,-14]
-#iterating statement
-for num in list1:
-#checking statement
-if num>=0:
-print(num);
+# Python program to print positive Numbers in a range 
+#Using forloop
+  
+# list of numbers 
+list1 = [12,-7,5,64,-14] 
+  
+# iterating each number in list 
+for num in list1: 
+      
+    # checking condition 
+    if num >= 0: 
+       print(num, end = " ") 
+        
+        
+        
+# list of numbers 
+list2 = [12,14,-95,3] 
+  
+# iteration
+for num in list2: 
+      
+    # checking condition 
+    if num >= 0: 
+       print(num, end = " ") 
+
+        
+#using while loop
+# Python program to print positive Numbers in a range
+  
+# list of numbers 
+list1 = [12,-7,5,64,-14] 
+num = 0
+    
+while(num < len(list1)): 
+      
+    # checking condition 
+    if list1[num] >= 0: 
+        print(list1[num], end = " ") 
+      
+    # increment 
+    num += 1
+    
+    
+    list2 = [12,14,-95,3] 
+num = 0
+    
+while(num < len(list1)): 
+      
+    # checking condition 
+    if list2[num] >= 0: 
+        print(list2[num], end = " ") 
+      
+    # increment 
+    num += 1
